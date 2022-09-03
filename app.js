95efe1f4bebac37ddc9448f122189b00f73765c4
Randyhall91 <randyrocks11@gmail.com>
2022-09-03 15:28:21 -0600

    Saturday Updates

--- app.js
+++ app.js
@@ -1,68 +1,249 @@
 let rune = 0
 let runesPerClick = 1
 let ashperSecond = 0
+let equipped = 0
+let greatRune = 0
+let bossesDefeated = 0
+// TODO 
+//     Change Weapons / ashes to buttons
+//     Change ashes to just add damage for boss fights
+//     Add Bosses that grant Great Runes on defeat
+//     Great Runes now generate runes instead of ashes
+
+//     Draw weapons based off Great Runes aquired
+//     Draw first boss if 2 weapons are aquired
+//     Draw the next off great runes aquired
+
 const weapons = [
+  {
+    name: "Basic Sword",
+    quantity: 0,
+    max: 2,
+    cost: 1,
+    runemultiplier: 1,
+    starter: true
+  },
   {
     name: "Dark Moon Greatsword",
     quantity: 0,
     max: 2,
     cost: 5,
-    runemultiplier: 5
+    runemultiplier: 5,
+    starter: false,
+    reward: 2
   },
   {
     name: "Sword of Night and Flame",
     quantity: 0,
     cost: 10,
     max: 2,
-    runemultiplier: 7
+    runemultiplier: 7,
+    starter: false,
+    reward: 1
   },
   {
     name: "Moonveil",
     quantity: 0,
     cost: 20,
     max: 2,
-    runemultiplier: 10
+    runemultiplier: 10,
+    starter: false,
+    reward: 3
   }
 ]
 const ashes = [
   {
+    name: "Wolves",
+    quantity: 0,
+    runepersecond: 2,
+    cost: 3,
+    reward: 0,
+    starter: true
+  },
+  {
     name: "Black Knife Tiche",
     quantity: 0,
-    cost: 30,
-    runepersecond: 5
+    runepersecond: 3,
+    cost: 3,
+    reward: 1,
+    starter: false
   },
   {
     name: "Mimic Tear",
     quantity: 0,
-    cost: 40,
-    runepersecond: 7
+    runepersecond: 4,
+    cost: 4,
+    reward: 2,
+    starter: false
   },
   {
     name: "Dung Eater Puppet",
     quantity: 0,
+    runepersecond: 5,
     cost: 50,
-    runepersecond: 10
+    reward: 3,
+    starter: false
   }
 ]
-//NOTE Purchase Upgrades
-function buyWeapon(weapon) {
-  const boughtweapon = weapons.find(w => w.name == weapon)
+const bosses = [
+  {
+    name: "Margit, the Fell Omen",
+    cost: 50,
+    starter: true,
+    reward: 0
+  },
+  {
+    name: "Godrick the Grafted",
+    cost: 150,
+    starter: false,
+    reward: 1
+  },
+  {
+    name: "Starscourge Radahn",
+    cost: 250,
+    starter: false,
+    reward: 2
+  }
+]
+//SECTION Draw Weapon Upgrades
+function drawWeapon() {
+  let weapon = document.getElementById('drawWeapon')
+  let template = ''
+
+  weapons.forEach(w => {
+    if (w.starter == true) {
+      template += `
+    <button onclick="buyWeapon('${w.name}')" class="fs-5 d-flex btn btn-info my-1">${w.name} | Cost: ${w.cost}
+        </button>
+    `
+    }
+    if (greatRune >= 1) {
+      if (w.reward == 1)
+        template += `
+    <button onclick="buyWeapon('${w.name}')" class="fs-5 d-flex btn btn-info my-1">${w.name} | Cost: ${w.cost}
+        </button>
+    `
+    }
+    if (greatRune >= 2) {
+      if (w.reward == 2)
+        template += `
+    <button onclick="buyWeapon('${w.name}')" class="fs-5 d-flex btn btn-info my-1">${w.name} | Cost: ${w.cost}
+        </button>
+    `
+    }
+    if (greatRune >= 3) {
+      if (w.reward == 3)
+        template += `
+    <button onclick="buyWeapon('${w.name}')" class="fs-5 d-flex btn btn-info my-1">${w.name} | Cost: ${w.cost}
+        </button>
+    `
+    }
+
+  })
+
+  // @ts-ignore
+  weapon.innerHTML = template
+
+}
+//SECTION Draw Ash Upgrades
+function drawAsh() {
+  let ash = document.getElementById('drawAsh')
+  let template = ''
+  ashes.forEach(a => {
+    if (a.starter == true) {
+      template += `
+    <button onclick="buyAsh('${a.name}')" class="fs-5 d-flex btn btn-info my-1">${a.name} | Cost: ${a.cost}
+        </button>
+    `
+    }
+    if (greatRune >= 1) {
+      if (a.reward == 1)
+        template += `
+        <button onclick="buyAsh('${a.name}')" class="fs-5 d-flex btn btn-info my-1">${a.name} | Cost: ${a.cost}
+        </button>
+        `
+    }
+    if (greatRune >= 2) {
+      if (a.reward == 2)
+        template += `
+    <button onclick="buyAsh('${a.name}')" class="fs-5 d-flex btn btn-info my-1">${a.name} | Cost: ${a.cost}
+        </button>
+        `
+    }
+    if (greatRune >= 3) {
+      if (a.reward == 3)
+        template += `
+        <button onclick="buyAsh('${a.name}')" class="fs-5 d-flex btn btn-info my-1">${a.name} | Cost: ${a.cost}
+        </button>
+        `
+    }
+
+  })
 
 
   // @ts-ignore
-  if (boughtweapon.quantity >= 2) {
+  ash.innerHTML = template
+
+}
+// SECTION Draw Bosses
+function drawBoss() {
+  let template = ''
+  let boss = document.getElementById('drawBoss')
+  bosses.forEach(b => {
+    if (b.starter == true) {
+      template += `
+    <button onclick="buyBoss('${b.name}')" class="fs-5 d-flex btn btn-info my-1">${b.name} | Cost: ${b.cost}
+        </button>
+    `
+    }
+    if (greatRune >= 1) {
+      if (b.reward == 1)
+        template += `
+        <button onclick="buyBoss('${b.name}')" class="fs-5 d-flex btn btn-info my-1">${b.name} | Cost: ${b.cost}
+        </button>
+        `
+    }
+    if (greatRune >= 2) {
+      if (b.reward == 2)
+        template += `
+        <button onclick="buyBoss('${b.name}')" class="fs-5 d-flex btn btn-info my-1">${b.name} | Cost: ${b.cost}
+        </button>
+        `
+    }
+    if (greatRune >= 3) {
+      if (b.reward == 3)
+        template += `
+        <button onclick="buyBoss('${b.name}')" class="fs-5 d-flex btn btn-info my-1">${b.name} | Cost: ${b.cost}
+        </button>
+        `
+    }
+
+  })
+  boss.innerHTML = template
+}
+
+
+
+
+
+//SECTION Purchase Upgrades
+function buyWeapon(weapon) {
+  const boughtweapon = weapons.find(w => w.name == weapon)
+
+  // @ts-ignore
+  if (equipped >= 2) {
     // @ts-ignore
-    boughtweapon.quantity = 2
+    equipped = 2
     return
   }
-
-
   // @ts-ignore
   if (rune >= boughtweapon.cost) {
     // @ts-ignore
     rune -= boughtweapon.cost
     // @ts-ignore
     boughtweapon.quantity++
+    equipped++
+    console.log(equipped);
     // @ts-ignore
     // console.log('you bought a weapon!', boughtweapon.name);
     // @ts-ignore
@@ -71,31 +252,12 @@ function buyWeapon(weapon) {
       boughtweapon.cost += 20
       // @ts-ignore
 
-      console.log('cost of', boughtweapon.name, boughtweapon.cost);
+      // console.log('cost of', boughtweapon.name, boughtweapon.cost);
     }
   }
+  update()
+}
 
-  drawWeaponCost(boughtweapon.name, boughtweapon.cost)
-  update()
-}
-function drawWeaponCost(name, cost) {
-  if (name == "Dark Moon Greatsword") {
-    let weaponCost = document.getElementById('DMweaponCost')
-    // @ts-ignore
-    weaponCost.innerText = cost
-  }
-  if (name == "Sword of Night and Flame") {
-    let weaponCost = document.getElementById('SNFweaponCost')
-    // @ts-ignore
-    weaponCost.innerText = cost
-  }
-  if (name == "Moonviel") {
-    let weaponCost = document.getElementById('MVweaponCost')
-    // @ts-ignore
-    weaponCost.innerText = cost
-  }
-  update()
-}
 
 function buyAsh(ash) {
   const boughtash = ashes.find(a => a.name == ash)
@@ -112,28 +274,29 @@ function buyAsh(ash) {
     console.log('you bought a Ash!', boughtash.name);
   }
   // @ts-ignore
-  drawAshCost(boughtash.name, boughtash.cost)
+
   update()
 }
-function drawAshCost(name, cost) {
-  if (name == "Black Knife Tiche") {
-    let weaponCost = document.getElementById('BKTashCost')
-    // @ts-ignore
-    weaponCost.innerText = cost
-  }
-  if (name == "Mimic Tear") {
-    let weaponCost = document.getElementById('MTashCost')
-    // @ts-ignore
-    weaponCost.innerText = cost
-  }
-  if (name == "Dung Eater Puppet") {
-    let weaponCost = document.getElementById('DEPashCost')
-    // @ts-ignore
-    weaponCost.innerText = cost
-  }
 
+function buyBoss(boss) {
+  const boughtBoss = bosses.find(b => b.name == boss)
+
+
+  if (rune >= boughtBoss.cost) {
+    rune -= boughtBoss.cost
+
+    greatRune++
+
+  }
+  update()
 }
-//NOTE On Click function
+
+
+
+
+
+
+//SECTION On Click function
 function farm() {
   let totaldmg = 1
   // @ts-ignore
@@ -155,10 +318,13 @@ function farm() {
 }
 
 
-//NOTE Ashes Interval
-function ashFarm() {
+//SECTION Rune gain Interval
+function runeFarm() {
 
   let sum = 0
+  if (greatRune >= 1) {
+    sum += greatRune * 10
+  }
   ashes.forEach(ash => {
     if (ash.quantity > 0) {
       sum += ash.runepersecond * ash.quantity
@@ -166,27 +332,37 @@ function ashFarm() {
   })
   ashperSecond = sum
   rune += sum
-  // console.log('ashfarm', rune);
+
   update()
 }
-setInterval(ashFarm, 3000)
-
-function drawWeapons() {
+setInterval(runeFarm, 1000)
+// SECTION Draw Weapons Ashes and # of Great Runes under Status
+function drawWeaponsStatus() {
   let template = ''
-  let weapon = document.getElementById('drawWeapons')
+  let weapon = document.getElementById('drawWeaponsStatus')
   weapons.forEach(w => {
     if (w.quantity > 0) {
       template += `
       <h5>${w.name} Quantity: <p>${w.quantity}</p></h5>
+      <button onclick="unequip('${w.name}')" class="btn btn-danger">Unequip</button>
       `
     }
     // @ts-ignore
     weapon.innerHTML = template
   })
+
 }
-function drawAshes() {
+function unequip(weapon) {
+  const wep = weapons.find(w => w.name == weapon)
+  // @ts-ignore
+  wep.quantity--
+  equipped--
+  update()
+}
+//NOTE UPDATE to Great rune status
+function drawAshesStatus() {
   let template = ''
-  let ash = document.getElementById('drawAshes')
+  let ash = document.getElementById('drawAshesStatus')
   ashes.forEach(a => {
     if (a.quantity > 0) {
       template += `
@@ -199,7 +375,13 @@ function drawAshes() {
 }
 
 
-//NOTE Update
+//TODO set max ashes to 2, add dissmiss button for ashes
+// Balance 
+
+
+
+
+//SECTION Update
 function update() {
   // @ts-ignore
 
@@ -213,6 +395,10 @@ function update() {
   let perash = document.getElementById('runespersecond')
   // @ts-ignore
   perash.innerText = ashperSecond
-  drawWeapons()
-  drawAshes()
+  drawWeaponsStatus()
+  drawWeapon()
+  drawAshesStatus()
+  drawAsh()
+  drawBoss()
 }
+update()
